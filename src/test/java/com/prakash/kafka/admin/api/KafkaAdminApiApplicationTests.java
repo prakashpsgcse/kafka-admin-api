c2ef77cc2ad07922485282f2e6d34e2aa4fe4773
prakashpsgcse <prakashpsgcse@gmail.com>
2020-11-20 20:39:50 +0530

    first commit

---
+++ src/test/java/com/prakash/kafka/admin/api/KafkaAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakash.kafka.admin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
